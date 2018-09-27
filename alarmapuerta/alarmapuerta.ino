5749858655338db9b5b4b00bc1b2b980e1fae520
SilviaYan <43188872+SilviaYan@users.noreply.github.com>
2018-09-27 12:09:18 -0500

    Add files via upload

--- alarmapuerta/alarmapuerta.ino
+++ alarmapuerta/alarmapuerta.ino
@@ -1,32 +1,34 @@
+//Tarea: Sistema de alarma para puerta
 
-int contacto = 2;
-int led = 13;
+int contacto = 2;           //pin del red switch
+int led = 13;               //pin del led
 
 void setup() {
-pinMode(contacto, INPUT);
+pinMode(contacto, INPUT);   
 pinMode(led, OUTPUT);  
 pinMode(8, OUTPUT);
 
 }
 
 void loop() {
-  digitalWrite(led,LOW);
+  digitalWrite(led,LOW);   
   noTone(8);
-  if(digitalRead(contacto)==LOW){
+  
+  if(digitalRead(contacto)==LOW){ // en caso de que el iman esten en contacto con el red switch se activara buzzer y un led simulando que la puerta esta abierta
  
-          tone(8, 523, 200);
+          tone(8, 523, 200);      // se indica el pin en el cual se encuentra el buzzer, la frecuencia y el tiempo que se desea dure el sonido
+          delay(100);             
+          tone(8, 622, 200);      
           delay(100);
-          tone(8, 622, 200);
+          digitalWrite(led,HIGH); //enciende 
           delay(100);
-          digitalWrite(led,HIGH);
-          delay(100);
-          digitalWrite(led,LOW);
+          digitalWrite(led,LOW);  //y apaga el led para que parpadee
           delay(100);
 
      
-  }else{
-          noTone(8);
-          digitalWrite(led,LOW);
+  }else{                         //en caso de que no se acerque el iman no sonara el buzzer ni encendera el led 
+          noTone(8);             //apagar el buzzer
+          digitalWrite(led,LOW); //apagar el led
   }
 
 }
